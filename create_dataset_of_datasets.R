1389f3c7f669c59d2b93341cfe9bdd2bf5bf56ad
Saghir Bashir <saghir@ilustat.com>
2017-11-28 17:35:18 +0000

    Update: all_ds is saved in "data" rather than created on the fly. Prevents problems for the users who don't have all the directories used.

---
+++ create_dataset_of_datasets.R
@@ -0,0 +1,50 @@
+# Libraries for datasets
+library(tidyverse)
+library(tibble)
+library(stringr)
+library(MASS)
+library(fivethirtyeight)
+library(gapminder)
+library(nycflights13)
+#library(Epi)
+library(stratification)
+library(lsmeans)
+library(SwissAir)
+library(eha)
+#library(MEMSS)
+#library(fda)
+library(bundesligR)
+library(boot)
+library(nlme)
+library(Ecdat)
+library(eurostat)
+library(europop)
+library(HistData)
+library(babynames)
+library(fueleconomy)
+library(nasaweather)
+
+# Identifying all the available datasets.
+all_ds_ls <- data()
+
+all_ds <- all_ds_ls$results %>%
+  tibble::as_tibble() %>%
+  dplyr::rename(Data_Orig = Item) %>%
+  dplyr::mutate(dataset = stringr::word(Data_Orig, 1)) %>%
+  dplyr::mutate(Class = purrr::invoke_map(class, as.list(parse(text = dataset)))) %>%
+  tidyr::unnest(Class) %>%
+  dplyr::select(Package, Data_Orig, Title, Class) %>%
+  dplyr::arrange(Package, Data_Orig, Class) %>%
+  dplyr::mutate(Val = Class) %>%
+  tidyr::spread(key = Class, value=Val, fill = "") %>%
+  tidyr::unite(Classes, c(-Package, -Data_Orig, -Title), sep= " ") 
+
+# Long version makes it easier to filter by class.
+all_long <- all_ds %>% 
+  mutate(class = strsplit(Classes, "\\s+")) %>%
+  unnest(class) %>%
+  filter(class != "") %>% 
+  mutate(class = as.factor(class)) 
+
+readr::write_rds(all_ds, path = "data/all_ds")
+readr::write_rds(all_long, path = "data/all_long")
--- global.R
+++ global.R
@@ -2,59 +2,8 @@ library(shiny)
 library(shinydashboard)
 library(DT)
 
-# Libraries for datasets
-library(tidyverse)
-library(tibble)
-library(stringr)
-library(forcats)
-
-library(MASS)
-library(fivethirtyeight)
-library(gapminder)
-library(nycflights13)
-#library(Epi)
-library(stratification)
-library(lsmeans)
-library(SwissAir)
-library(eha)
-#library(MEMSS)
-#library(fda)
-library(bundesligR)
-library(boot)
-library(nlme)
-library(Ecdat)
-library(eurostat)
-library(europop)
-library(HistData)
-library(babynames)
-library(fueleconomy)
-library(nasaweather)
-#library(UScensus2010)
-
-
-# Identifying all the available datasets.
-all_ds_ls <- data(verbose = TRUE)
-
-all_ds <- all_ds_ls$results %>%
-  tibble::as_tibble() %>%
-  dplyr::rename(Data_Orig = Item) %>%
-  dplyr::mutate(dataset = stringr::word(Data_Orig, 1)) %>%
-  dplyr::mutate(Class = purrr::invoke_map(class, as.list(parse(text = dataset)))) %>%
-  tidyr::unnest(Class) %>%
-  dplyr::select(Package, Data_Orig, Title, Class) %>%
-  dplyr::arrange(Package, Data_Orig, Class) %>%
-  dplyr::mutate(Val = Class) %>%
-  tidyr::spread(key = Class, value=Val, fill = "") %>%
-  tidyr::unite(Classes, c(-Package, -Data_Orig, -Title), sep= " ") 
-
-# Long version makes it easier to filter by class.
-all_long <- all_ds %>% 
-  mutate(class = strsplit(Classes, "\\s+")) %>%
-  unnest(class) %>%
-  filter(class != "") %>% 
-  mutate(class = as.factor(class)) 
+readr::read_rds("data/all_ds")
+readr::read_rds("data/all_long")
 
 uniqueClasses <- fct_unique(all_long$class)
-
-uniquePackages <- unique(all_ds$Package) %>% 
-  sort()
+uniquePackages <- unique(all_ds$Package) %>% sort()
